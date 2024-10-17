0ebd5282d2c29e3b5bade0726d49a253fa16b6a9
15446-rus75 <vladabramov06@yandex.ru>
2024-10-17 11:17:02 +0300

    declaration of functions

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+
+int** createMatrix(size_t m, size_t n);
+void destroyMatrix (int** t, size_t m, size_t n);
+
 int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
+  int** t = createMatrix(M, N);
 
+  destroyMatrix(t, M, N);
   std::cout << M << " " << N << "\n";
 }
