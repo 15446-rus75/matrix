54c6b97cc6504664b3c53ac2a8eea3de332deccd
15446-rus75 <vladabramov06@yandex.ru>
2024-12-27 22:20:27 +0300

    delete old matrix struct

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,5 @@
 #include "matrix.hpp"
 #include <iostream>
-#include <exception>
 
 void destroyMatrix (int **t, size_t m, size_t n)
 {
@@ -56,80 +55,4 @@ void writeMatrix(const int * const * t, size_t m, size_t n)
     }
     std::cout << "\n";
   }
-}
-
-Matrix::Matrix(size_t m, size_t n):
-  t_(createMatrix(m, n)),
-  m_(m),
-  n_(n)
-{}
-
-Matrix::Matrix(size_t m, size_t n, int value):
-  Matrix(m, n)
-{
-  for (size_t i = 0; i < m; ++i)
-  {
-    for (size_t j = 0; j < n; ++j)
-    {
-      t_[i][j] = value;
-    }
-  }
-}
-
-Matrix::Matrix(const Matrix &mtx):
-  Matrix(mtx.m_, mtx.n_)
-{
-  for (size_t i = 0; i < mtx.m_; ++i)
-  {
-    for (size_t j = 0; j < mtx.n_; ++j)
-    {
-      t_[i][j] = mtx.t_[i][j];
-    }
-  }
-}
-
-Matrix::~Matrix()
-{
-  ::destroyMatrix(t_, m_, n_);
-}
-
-void Matrix::read()
-{
-  ::readMatrix(t_, m_, n_);
-}
-
-void Matrix::write() const
-{
-  ::writeMatrix(t_, m_, n_);
-}
-
-size_t Matrix::getColumns() const
-{
-  return n_;
-}
-
-size_t Matrix::getRows() const
-{
-  return m_;
-}
-
-void Matrix::filling(int a)
-{
-  for (size_t i = 0; i < m_; ++i)
-  {
-    for (size_t j = 0; j < n_; ++j)
-    {
-      t_[i][j] = a;
-    }
-  }
-}
-
-void Matrix::transform(size_t m, size_t n)
-{
-  int **old_t_ = t_;
-  t_ = createMatrix(m, n);
-  destroyMatrix(old_t_, m_, n_);
-  m_ = m;
-  n_ = n;
-  filling(0);
 }
--- matrix.hpp
+++ matrix.hpp
@@ -5,22 +5,4 @@ int** createMatrix(size_t m, size_t n);
 void destroyMatrix(int **t, size_t m, size_t n);
 void readMatrix(int **t, size_t m, size_t n);
 void writeMatrix(const int * const *t, size_t m, size_t n);
-struct Matrix
-{
-  Matrix();
-  ~Matrix();
-  Matrix(size_t m, size_t n);
-  Matrix(size_t m, size_t n, int value);
-  Matrix(const Matrix &mtx);
-  void read();
-  void write() const;
-  size_t getRows() const;
-  size_t getColumns() const;
-  void filling (int a);
-  void transform(size_t m, size_t n);
-  private:
-    int **t_;
-    size_t m_;
-    size_t n_;
-};
 #endif
