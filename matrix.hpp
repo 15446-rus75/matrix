e1c68cb6bae1901250132769e80992e3868f4caa
15446-rus75 <vladabramov06@yandex.ru>
2025-07-13 16:18:17 +0300

    update matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -1,4 +1,22 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+namespace abramov
+{
+  struct Matrix
+  {
+    Matrix();
+    Matrix(const Matrix &matrix);
+    Matrix(Matrix &&matrix);
+    Matrix(size_t m, size_t n, int value);
+    Matrix(size_t m, size_t n, const int *values);
+    ~Matrix();
+    Matrix &operator=(const Matrix &matrix);
+    Matrix &operator=(Matrix &&matrix);
+  private:
+    int **data;
+    size_t rows;
+    size_t cols;
+  };
+}
 #endif
