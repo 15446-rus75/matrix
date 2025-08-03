0f1e548c8d8978ffc883839a2d9e08706c28af0d
15446-rus75 <vladabramov06@yandex.ru>
2025-08-03 11:54:38 +0300

    rewrite tests without bool b and add test for trace

--- test-main.cpp
+++ test-main.cpp
@@ -11,8 +11,7 @@ BOOST_AUTO_TEST_CASE(fill_constructor)
 {
   abramov::Matrix matrix(3, 3, 0);
   abramov::Matrix res = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
-  bool b = matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(data_constructor)
@@ -25,8 +24,7 @@ BOOST_AUTO_TEST_CASE(data_constructor)
   }
   abramov::Matrix matrix(3, 3, vals);
   abramov::Matrix res = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
-  bool b = matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == res));
   delete[] vals;
 }
 
@@ -41,8 +39,7 @@ BOOST_AUTO_TEST_CASE(copy_constructor)
   abramov::Matrix tmp(3, 3, vals);
   abramov::Matrix matrix(tmp);
   abramov::Matrix res = { { 0, 1, 2 }, { 3, 4, 5 }, { 6, 7, 8 } };
-  bool b = matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == res));
   delete[] vals;
 }
 
@@ -55,16 +52,14 @@ BOOST_AUTO_TEST_CASE(copy_operator_equal)
 {
   abramov::Matrix matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
   abramov::Matrix copy_matrix = matrix;
-  bool b = matrix == copy_matrix;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == copy_matrix));
 }
 
 BOOST_AUTO_TEST_CASE(move_operator_equal)
 {
   abramov::Matrix matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
   abramov::Matrix copy_matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-  bool b = matrix == copy_matrix;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == copy_matrix));
 }
 
 BOOST_AUTO_TEST_CASE(operator_plus_equal)
@@ -73,8 +68,7 @@ BOOST_AUTO_TEST_CASE(operator_plus_equal)
   abramov::Matrix copy_matrix = matrix;
   matrix += copy_matrix;
   abramov::Matrix res = { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
-  bool b = matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_binary_plus)
@@ -83,16 +77,14 @@ BOOST_AUTO_TEST_CASE(operator_binary_plus)
   abramov::Matrix copy_matrix = matrix;
   abramov::Matrix new_matrix = matrix + copy_matrix;
   abramov::Matrix res = { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
-  bool b = new_matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((new_matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_unary_plus)
 {
   abramov::Matrix matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
   abramov::Matrix copy_matrix = +matrix;
-  bool b = matrix == copy_matrix;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == copy_matrix));
 }
 
 BOOST_AUTO_TEST_CASE(operator_minus_equal)
@@ -101,8 +93,7 @@ BOOST_AUTO_TEST_CASE(operator_minus_equal)
   abramov::Matrix copy_matrix = matrix;
   matrix -= copy_matrix;
   abramov::Matrix res(3, 3, 0);
-  bool b = matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_binary_minus)
@@ -111,8 +102,7 @@ BOOST_AUTO_TEST_CASE(operator_binary_minus)
   abramov::Matrix copy_matrix = matrix;
   abramov::Matrix res = matrix - copy_matrix;
   abramov::Matrix zero(3, 3, 0);
-  bool b = res == zero;
-  BOOST_TEST(b);
+  BOOST_TEST((res == zero));
 }
 
 BOOST_AUTO_TEST_CASE(operator_unary_minus)
@@ -120,8 +110,7 @@ BOOST_AUTO_TEST_CASE(operator_unary_minus)
   abramov::Matrix matrix(3, 3, 1);
   abramov::Matrix anti_matrix = -matrix;
   abramov::Matrix res(3, 3, -1);
-  bool b = anti_matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((anti_matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_multiple_equal_matrix)
@@ -130,8 +119,7 @@ BOOST_AUTO_TEST_CASE(operator_multiple_equal_matrix)
   abramov::Matrix c = { { 6, 5, 4 }, { 3, 2, 1 } };
   a *= c;
   abramov::Matrix res = { { 12, 9, 6 }, { 30, 23, 16 }, { 48, 37, 26 }, { 66, 51, 36 } };
-  bool b = a == res;
-  BOOST_TEST(b);
+  BOOST_TEST((a == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_multiple_matrix_matrix)
@@ -140,8 +128,7 @@ BOOST_AUTO_TEST_CASE(operator_multiple_matrix_matrix)
   abramov::Matrix c = { { 6, 5, 4 }, { 3, 2, 1 } };
   abramov::Matrix d = a * c;
   abramov::Matrix res = { { 12, 9, 6 }, { 30, 23, 16 }, { 48, 37, 26 }, { 66, 51, 36 } };
-  bool b = d == res;
-  BOOST_TEST(b);
+  BOOST_TEST((d == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_multiple_equal_scalar)
@@ -149,8 +136,7 @@ BOOST_AUTO_TEST_CASE(operator_multiple_equal_scalar)
   abramov::Matrix matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
   matrix *= 2;
   abramov::Matrix res = { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
-  bool b = matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_multiple_matrix_scalar)
@@ -158,8 +144,7 @@ BOOST_AUTO_TEST_CASE(operator_multiple_matrix_scalar)
   abramov::Matrix matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
   abramov::Matrix double_matrix = matrix * 2;
   abramov::Matrix res = { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
-  bool b = double_matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((double_matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(operator_multiple_scalar_matrix)
@@ -167,8 +152,7 @@ BOOST_AUTO_TEST_CASE(operator_multiple_scalar_matrix)
   abramov::Matrix matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
   abramov::Matrix double_matrix = 2 * matrix;
   abramov::Matrix res = { { 2, 4, 6 }, { 8, 10, 12 }, { 14, 16, 18 } };
-  bool b = double_matrix == res;
-  BOOST_TEST(b);
+  BOOST_TEST((double_matrix == res));
 }
 
 BOOST_AUTO_TEST_CASE(transpose)
@@ -176,8 +160,7 @@ BOOST_AUTO_TEST_CASE(transpose)
   abramov::Matrix matrix = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
   abramov::Matrix t = matrix.transpose();
   abramov::Matrix res = { { 1, 3, 5, 7 }, { 2, 4, 6, 8 } };
-  bool b = t == res;
-  BOOST_TEST(b);
+  BOOST_TEST((t == res));
 }
 
 BOOST_AUTO_TEST_CASE(determinant)
@@ -192,14 +175,19 @@ BOOST_AUTO_TEST_CASE(determinant)
   BOOST_TEST(m4.determinant() == 0);
 }
 
+BOOST_AUTO_TEST_CASE(trace)
+{
+  abramov::Matrix m = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+  BOOST_TEST(m.trace() == 15);
+}
+
 BOOST_AUTO_TEST_CASE(horizontal_concat)
 {
   abramov::Matrix m1 = { { 1, 2, 3 }, { 4, 5, 6 } };
   abramov::Matrix m2 = { { 7, 8 }, { 9, 10 }, { 11, 12 } };
   abramov::Matrix conc = m1.horizontalConcat(m1, m2);
   abramov::Matrix res = { { 1, 2, 3, 7, 8 }, { 4, 5, 6, 9, 10 }, { 0, 0, 0, 11, 12 } };
-  bool b = conc == res;
-  BOOST_TEST(b);
+  BOOST_TEST((conc == res));
 }
 
 BOOST_AUTO_TEST_CASE(vertical_concat)
@@ -208,8 +196,7 @@ BOOST_AUTO_TEST_CASE(vertical_concat)
   abramov::Matrix m2 = { { 7, 8 }, { 9, 10 }, { 11, 12 } };
   abramov::Matrix conc = m1.verticalConcat(m1, m2);
   abramov::Matrix res = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 }, { 9, 10, 0 }, { 11, 12, 0 } };
-  bool b = conc == res;
-  BOOST_TEST(b);
+  BOOST_TEST((conc == res));
 }
 
 BOOST_AUTO_TEST_CASE(diagonal_concat)
@@ -218,8 +205,7 @@ BOOST_AUTO_TEST_CASE(diagonal_concat)
   abramov::Matrix m2 = { { 7, 8 }, { 9, 10 }, { 11, 12 } };
   abramov::Matrix conc = m1.diagonalConcat(m1, m2);
   abramov::Matrix res = { { 1, 2, 3, 0, 0 }, { 4, 5, 6, 0, 0 }, { 0, 0, 0, 7, 8 }, { 0, 0, 0, 9, 10 }, { 0, 0, 0, 11, 12 } };
-  bool b = conc == res;
-  BOOST_TEST(b);
+  BOOST_TEST((conc == res));
 }
 
 BOOST_AUTO_TEST_CASE(kronecker_product)
@@ -228,6 +214,5 @@ BOOST_AUTO_TEST_CASE(kronecker_product)
   abramov::Matrix m2 = { { 0, 5 }, { 6, 7 } };
   abramov::Matrix prod = m1.kroneckerProduct(m1, m2);
   abramov::Matrix res = { { 0, 5, 0, 10 }, { 6, 7, 12, 14 }, { 0, 15, 0, 20 }, { 18, 21, 24, 28 } };
-  bool b = prod == res;
-  BOOST_TEST(b);
+  BOOST_TEST((prod == res));
 }
