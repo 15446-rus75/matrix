7cdd43dc836121f2cdee84f8af641fce9e442612
15446-rus75 <vladabramov06@yandex.ru>
2025-08-28 22:10:47 +0300

    add tests for dot, cross, triple, norm, normalized

--- test-vector.cpp
+++ test-vector.cpp
@@ -123,6 +123,47 @@ BOOST_AUTO_TEST_CASE(operator_multiply_scalar_vector)
   BOOST_TEST((vect == vect2));
 }
 
+BOOST_AUTO_TEST_CASE(dot)
+{
+  abramov::Vector< int, N > vect1 = { 1, 2, 3 };
+  abramov::Vector< int, N > vect2 = { -2, 0, 6 };
+  int res = vect1.dot(vect2);
+  BOOST_TEST(res == 16);
+}
+
+BOOST_AUTO_TEST_CASE(cross)
+{
+  abramov::Vector< int, N > vect1 = { 1, 2, 3 };
+  abramov::Vector< int, N > vect2 = { -2, 0, 6 };
+  abramov::Vector< int, N > vect3 = vect1.cross(vect2);
+  abramov::Vector< int, N > vect = {12, -12, 4 };
+  BOOST_TEST((vect == vect3));
+}
+
+BOOST_AUTO_TEST_CASE(triple)
+{
+  abramov::Vector< int, N > vect1 = { 1, 2, 3 };
+  abramov::Vector< int, N > vect2 = { -2, 0, 6 };
+  abramov::Vector< int, N > vect3 = { 4, 2, 1 };
+  int res = vect1.triple(vect2, vect3);
+  BOOST_TEST(res == 28);
+}
+
+BOOST_AUTO_TEST_CASE(norm)
+{
+  abramov::Vector< int, N > vect = { 3, 4, 0 };
+  double res = vect.norm();
+  BOOST_TEST(res == 5.0);
+}
+
+BOOST_AUTO_TEST_CASE(normalized)
+{
+  abramov::Vector< int, N > vect1 = { 3, 4, 0 };
+  abramov::Vector< double, N > vect2 = vect1.normalized();
+  abramov::Vector< double, N > vect = { 0.6, 0.8, 0.0 };
+  BOOST_TEST((vect2 == vect));
+}
+
 BOOST_AUTO_TEST_CASE(equal_operator)
 {
   abramov::Vector< double, N > vect1 = { 1.0, 2.0, 3.0 };
